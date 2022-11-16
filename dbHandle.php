0bf4bea9afbf3324641922435675e31593b12f7a
Pasindu Bhanuka <86610640+Bhanuka-Rankothge@users.noreply.github.com>
2022-11-16 17:47:14 +0530

    database check added

--- dbHandle.php
+++ dbHandle.php
@@ -1,10 +1,37 @@
 <?php
 //  Check user availability
 function userAvailabilityCheck(string $usr, string $psw){
-    if($usr == "Starme" && $psw == "test"){
-        return true;
-    }else{
+
+    $servername = "localhost";
+    $username = "test";
+    $password = "test";
+    $dbname = "starmeapp";
+
+        // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+        //  Query
+    $sql = "SELECT userID, username, password FROM users";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+
+        //  If user available
+        while($row = $result->fetch_assoc()) {
+            if($usr == $row["username"] && $row["password"]){
+                return true;
+            }
+        }
+    } else {
+
         return false;
     }
+    $conn->close();
+
 }
 ?>
