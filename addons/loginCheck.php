e9fc5318512711b37a9b0576ff6afe42c580f2b5
Pasindu Bhanuka <86610640+Bhanuka-Rankothge@users.noreply.github.com>
2022-11-17 14:50:18 +0530

    added loginCheck.php and regCheck.php.

---
+++ addons/loginCheck.php
@@ -0,0 +1,37 @@
+<?php
+
+    //  Include freqently used code
+include("dbHandle.php");
+session_destroy();
+    //  Starts the session
+session_start();
+  
+    //  Copy post values to session
+$_SESSION["username"] = $_POST["username"];
+$_SESSION["password"] = $_POST["password"];
+
+$_SESSION["msg"] = "Test";
+
+    //  Assign to variables
+$user = $_SESSION["username"];
+$pass = $_SESSION["password"];
+
+$available = userAvailabilityCheck($user, $pass);
+
+    //  Empty Check
+if($user != null && $pass != null){
+
+        //  Availability Check
+    if($available){
+        $_SESSION["msg"] = "Logged in successfully.";
+        header("Location : ../index.php");
+    }
+    else{
+        $_SESSION["msg"] = "Username and Password doesn't match.";
+        header("Location : ../index.php");
+    }
+}else{
+    $_SESSION["msg"] = "Please fill all fields";
+    header("Location : ../index.php");
+}
+?>
